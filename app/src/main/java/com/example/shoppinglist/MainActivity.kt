30b3458b1bd40d6b4d473caf8d99270c53cf1a1d
salman <salmanpigi@gmail.com>
2021-02-21 02:36:04 +0700

    fix navigation

--- app/src/main/java/com/example/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/MainActivity.kt
@@ -1,16 +1,12 @@
 package com.example.shoppinglist
 
-import android.R
-import android.os.Bundle
-import android.view.MenuItem
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
-
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var formFragment: AddFormFragment
     private lateinit var itemListFragment: ItemListFragment
@@ -19,43 +15,20 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn_createItem.setOnClickListener(this)
-        btn_listItem.setOnClickListener(this)
         formFragment = AddFormFragment()
         itemListFragment = ItemListFragment()
 
-        supportFragmentManager.beginTransaction()
-            .add(R.id.fragmentContainer, itemListFragment).commit()
+        switchFragment(itemListFragment)
 
-//        bottom_navigation.setOnNavigationItemSelectedListener(navListener)
-
-    }
-
-//    private val navListener: BottomNavigationView.OnNavigationItemSelectedListener =
-//        BottomNavigationView.OnNavigationItemSelectedListener { item ->
-//            var selectedFragment: Fragment? = null
-//            when (item.itemId) {
-//                nav -> selectedFragment = HomeFragment()
-//                R.id.nav_favorites -> selectedFragment = FavoritesFragment()
-//                R.id.nav_search -> selectedFragment = SearchFragment()
-//            }
-//            supportFragmentManager.beginTransaction().replace(
-//                R.id.fragment_container,
-//                selectedFragment!!
-//            ).commit()
-//            true
-//        }
-
-    override fun onClick(v: View?) {
-        when (v) {
-            btn_listItem -> {
-                switchFragment(itemListFragment)
+        bottom_nav.setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.nav_add -> switchFragment(itemListFragment)
+                R.id.nav_itemList -> switchFragment(formFragment)
             }
-            btn_createItem -> {
-                switchFragment(formFragment)
-            }
+            true
         }
     }
+
 
     private fun switchFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, fragment).commit()
