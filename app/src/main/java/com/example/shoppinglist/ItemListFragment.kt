1e10c530ff1eec46320fdf643517fa5a549c4b99
salman <salmanpigi@gmail.com>
2021-02-21 00:35:56 +0700

    can replace fragment

--- app/src/main/java/com/example/shoppinglist/ItemListFragment.kt
+++ app/src/main/java/com/example/shoppinglist/ItemListFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class AddFormFragment : Fragment() {
+
+class ItemListFragment : Fragment() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -15,11 +17,11 @@ class AddFormFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_add_form, container, false)
+        return inflater.inflate(R.layout.fragment_item_list, container, false)
     }
 
     companion object {
         @JvmStatic
-        fun newInstance() = AddFormFragment()
+        fun newInstance() = ItemListFragment()
     }
 }
--- app/src/main/java/com/example/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.shoppinglist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var formFragment: AddFormFragment
+    private lateinit var itemListFragment: ItemListFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_createItem.setOnClickListener(this)
+        btn_listItem.setOnClickListener(this)
+
+        formFragment = AddFormFragment()
+        itemListFragment = ItemListFragment()
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragmentContainer, itemListFragment).commit()
+
+    }
+
+    override fun onClick(v: View?) {
+        when (v) {
+            btn_listItem -> {
+                switchFragment(itemListFragment)
+            }
+            btn_createItem -> {
+                switchFragment(formFragment)
+            }
+        }
+    }
+
+    private fun switchFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, fragment).commit()
     }
 }
