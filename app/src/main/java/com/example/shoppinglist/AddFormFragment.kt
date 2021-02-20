10200e69d3cfdc60ee272b0cd7902d16d684bf6b
salman <salmanpigi@gmail.com>
2021-02-20 11:54:03 +0700

    initial

---
+++ app/src/main/java/com/example/shoppinglist/AddFormFragment.kt
@@ -0,0 +1,25 @@
+package com.example.shoppinglist
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class AddFormFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_add_form, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) = AddFormFragment()
+    }
+}
