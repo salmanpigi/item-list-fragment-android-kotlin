772efb6a8f3d08ef3440d50abb0091c45f56d271
salman <salmanpigi@gmail.com>
2021-02-22 23:32:26 +0700

    fixx

--- app/src/main/java/com/example/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/MainActivity.kt
@@ -3,17 +3,13 @@ package com.example.shoppinglist
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.shoppinglist.`interface`.ServiceInterface
 import com.example.shoppinglist.enntities.Item
-import com.example.shoppinglist.fragment.AddFormFragment
-import com.example.shoppinglist.fragment.ItemListFragment
 import com.example.shoppinglist.utils.ItemList
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), ServiceInterface {
 
