80ad6af1177150df9510de40577989deb89578a4
salman <salmanpigi@gmail.com>
2021-02-22 23:30:44 +0700

    fix

--- app/src/main/java/com/example/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/MainActivity.kt
@@ -2,33 +2,33 @@ package com.example.shoppinglist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.shoppinglist.`interface`.ServiceInterface
 import com.example.shoppinglist.enntities.Item
 import com.example.shoppinglist.fragment.AddFormFragment
 import com.example.shoppinglist.fragment.ItemListFragment
 import com.example.shoppinglist.utils.ItemList
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), ServiceInterface {
 
-    private lateinit var formFragment: AddFormFragment
-    private lateinit var itemListFragment: ItemListFragment
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
+    lateinit var bottomNavigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        formFragment = AddFormFragment()
-        itemListFragment = ItemListFragment()
-
-        bottom_nav.setOnNavigationItemSelectedListener {
-            when(it.itemId) {
-                R.id.nav_add -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_global_addFormFragment)
-                R.id.nav_itemList -> findNavController(R.id.nav_host_fragment).navigate(R.id.action_global_itemListFragment)
-            }
-            true
-        }
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+        bottomNavigationView = findViewById(R.id.bottom_nav)
+        bottomNavigationView.setupWithNavController(navController)
     }
 
     override fun addItem(item: Item) {
