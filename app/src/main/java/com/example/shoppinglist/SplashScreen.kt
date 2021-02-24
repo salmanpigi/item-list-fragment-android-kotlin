6e0d3677bd38c2578d31f272a3d201686711b722
salman <salmanpigi@gmail.com>
2021-02-24 14:50:56 +0700

    add splash screen

---
+++ app/src/main/java/com/example/shoppinglist/SplashScreen.kt
@@ -0,0 +1,36 @@
+package com.example.shoppinglist
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_sreen)
+
+        Handler().postDelayed(Runnable {
+            val i = Intent(this, MainActivity::class.java)
+            this.startActivity(i)
+            this.finish()
+        }, 5000)
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) hideSystemgUI()
+    }
+
+    private fun hideSystemUI() {
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+    }
+}
